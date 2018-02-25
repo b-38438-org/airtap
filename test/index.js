4b0db619f0e7e9b1e973ea9f559e4dcbed745899
Vincent Weevers <mail@vincentweevers.nl>
2018-02-25 18:58:22 +0100

    run unit tests before integration tests (#98)

--- test/index.js
+++ test/index.js
@@ -1,11 +1,13 @@
-require('./integration/tape-sauce')
-require('./integration/tape-phantom')
-require('./integration/tape-electron')
 require('./unit/aggregate-browsers')
 require('./unit/browserify-builder')
 require('./unit/capabilities')
 require('./unit/flatten-browser')
+
 require('./unit/abstract-browser')
 require('./unit/electron-browser')
 require('./unit/phantom-browser')
 require('./unit/sauce-browser')
+
+require('./integration/tape-electron')
+require('./integration/tape-phantom')
+require('./integration/tape-sauce')
